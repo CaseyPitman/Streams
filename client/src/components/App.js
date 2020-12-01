8f9fa27a892c5c5499ffce736a6a287bf2f6a049
Casey The Great <casey@caseypitman.com>
2020-12-01 13:21:40 -0600

    Basic BrowserRouter setup.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { BrowserRouter, Route, Link} from "react-router-dom";
 
+const PageOne = () => {
+
+  return <h1>Page One</h1>
+
+}
+
+const PageTwo = () => {
+  return <h1>Page Two</h1>
+}
+
+
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path = '/' exact component = {PageOne}/>
+          <Route path = '/pagetwo' component = {PageTwo}/>
+        </div>
+      </BrowserRouter>
+    </div>
+
+
+  )
+
+
+
+
 };
 
 export default App;
