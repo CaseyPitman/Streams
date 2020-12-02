9c0e82a8f54658048d792145edb50fd9fdd22111
Casey The Great <casey@caseypitman.com>
2020-12-02 09:23:57 -0600

    Created Header component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,34 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Link} from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-
-  return <h1>Page One</h1>
-
-}
-
-const PageTwo = () => {
-  return <h1>Page Two</h1>
-}
-
+// Components
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <div>
-          <Route path = '/' exact component = {PageOne}/>
-          <Route path = '/pagetwo' component = {PageTwo}/>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamCreate} />
+          <Route path='/streams/edit' component={StreamEdit} />
+          <Route path='/streams/delete' component={StreamDelete} />
+          <Route path='/streams/show' component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
-
-
-  )
-
-
-
-
+  );
 };
 
 export default App;
