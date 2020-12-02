5b31abb7b701bc40aa45d10d440f6225201a7e7a
Casey The Great <casey@caseypitman.com>
2020-12-02 09:36:31 -0600

    Added links to Header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-   return <div>Header</div>
-}
+  return (
+    <div className='ui secondary pointing menu'>
+      <Link to='/' className='item'>
+        McStreamy
+      </Link>
+      <div className='right menu'>
+        <Link to='/' className='item'>
+          All Streams
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
