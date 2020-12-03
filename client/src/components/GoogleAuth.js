b859bb2705e0c9cc5959c15290f1bc0402620511
Casey The Great <casey@caseypitman.com>
2020-12-03 16:53:50 -0600

    GoogleAuth : basic sign in function achieved (via console).

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "177232814199-80h8v6jgj0pnj7ip2philfh7c24a9n7q.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "177232814199-80h8v6jgj0pnj7ip2philfh7c24a9n7q.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Dunno</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in</div>;
+    } else {
+      return <div>Not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
