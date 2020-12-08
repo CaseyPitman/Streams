d5e5da82555432768a7a923e2635eddcdc9adfb7
Casey The Great <casey@caseypitman.com>
2020-12-07 20:02:12 -0600

    CreateStream: created validate function to handle input errors.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -12,20 +12,45 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+
+
   render() {
     return (
-      <form className='ui form'>
+      <form
+        className='ui form'
+        onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name='title' component={this.renderInput} label='Enter Title' />
         <Field
           name='description'
           component={this.renderInput}
           label='Enter Description'
         />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }
 }
 
+
+const validate = (formValues) => {
+  const errors = {}
+  if (!formValues.title){
+    //only run if user did not enter a title
+    errors.title = "You must enter a title."
+  }
+  if (!formValues.description){
+    //only run if user did enter a description
+    errors.title = "You must enter a description."
+  }
+  return errors;
+}
+
 export default reduxForm({
   form: "streamCreate",
 })(StreamCreate);
+
+
