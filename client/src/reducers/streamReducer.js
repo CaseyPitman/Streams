be8f0a3694eceeb62132080a25a9b4c4586b3600
Casey The Great <casey@caseypitman.com>
2020-12-10 11:37:54 -0600

    streamReducer: added case for fetchStreams. Added streamReducer to combineReducers.

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -10,6 +10,9 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
 
@@ -22,7 +25,6 @@ export default (state = {}, action) => {
     case DELETE_STREAM:
       return _.omit(state, action.payload);
 
-    case FETCH_STREAMS:
     default:
       return state;
   }
