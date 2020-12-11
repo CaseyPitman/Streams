cc014cd34808b9a8e116b1f308187fe57f5b2398
Casey The Great <casey@caseypitman.com>
2020-12-11 11:05:06 -0600

    Conditionally rendered edit and delete buttons on stream list items when user is logged in and is the creator of a stream.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,14 +7,22 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  
-
-
+  renderAdmin = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  };
 
   renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             <h3 style={{ marginBottom: "0px" }}>{stream.title}</h3>
@@ -23,8 +31,7 @@ class StreamList extends React.Component {
         </div>
       );
     });
-  }
-
+  };
 
 
   render() {
@@ -33,12 +40,16 @@ class StreamList extends React.Component {
         <h2>Streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
       </div>
+
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
